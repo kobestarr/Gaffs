75b7303dd5b8bfb830c865b07895f85b23bd21d0
Kobi Omenaka <kobestarr@Kobestarrs-RetinaMBP.local>
2014-01-23 12:07:16 +0000

    set up rails for my gaffrenter project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gaffrenter::Application.config.secret_key_base = 'bb2a585835f3dba6904817103321f13bdd61cacb129696bfc0226410b02b2474185b881deb9180a1a969f5f174007671a2b02092099644e0938df32afb90891c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gaffrenter::Application.config.session_store :cookie_store, key: '_gaffrenter_session'
